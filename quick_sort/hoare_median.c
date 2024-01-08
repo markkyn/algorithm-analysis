b905ea46f27068d6f537f7d715bee78d933be649
markkyn <marcos.gabriel@ossirian.com>
2024-01-07 22:06:34 -0300

    fix: hoare median-of-three, working  ( needs review on call count)

--- quick_sort/hoare_median.c
+++ quick_sort/hoare_median.c
@@ -5,33 +5,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+uint32_t swap_count, call_count;
+
 void swap(int32_t *array_ptr, int source_pos, int dest_pos)
 {
+    swap_count++;
+
     int32_t temp = array_ptr[dest_pos];
 
     array_ptr[dest_pos] = array_ptr[source_pos];
     array_ptr[source_pos] = temp;
 }
 
-int32_t median(int a, int b, int c, int32_t n)
+int32_t median(int a, int b, int c, int32_t n, int32_t lo)
 {
+    call_count++;
+
     if ((a >= b && a <= c) || (a <= b && a >= c))
-        return n / 4; // a
+        return lo + (n / 4); // a
     else if ((b >= a && b <= c) || (b <= a && b >= c))
-        return n / 2; // b
+        return lo + (n / 2); // b
     else
-        return 3 * n / 4; // c
+        return lo + (3 * n / 2); // c
 }
 
 int32_t haore_median(int32_t *vector, int32_t low, int32_t high) // i = first position ; j = last position
 {
+    call_count++;
+
     int32_t n = high - low + 1;
 
-    int32_t a = vector[n / 4], b = vector[n / 2], c = vector[(3 * n) / 4];
+    int32_t a = vector[low + (n / 4)], b = vector[low + (n / 2)], c = vector[low + (3 * n / 4)];
 
-    int32_t k = median(a, b, c, n);
+    int32_t k = median(a, b, c, n, low);
 
-    // Arbitrary Pivot.
+    swap(vector, low, k);
+
+    // Median-of-three Pivot.
     int32_t P = vector[low], x = low - 1, y = high + 1;
 
     while (true)
@@ -49,7 +59,6 @@ int32_t haore_median(int32_t *vector, int32_t low, int32_t high) // i = first po
 
 void recursive_quicksort(int32_t *vector, int32_t start, int32_t end)
 {
-
     if (start < end)
     {
         int32_t pivot = haore_median(vector, start, end);
@@ -65,7 +74,6 @@ int32_t *vector_ptr, vector_len, vectors_count;
 
 int main(int argc, char *argv[])
 {
-
     if (argc != 3)
     {
         perror("Quantidade incorreta de argumentos!");
@@ -84,14 +92,21 @@ int main(int argc, char *argv[])
 
         for (int j = 0; j < vector_len; j++)
             fscanf(input_fp, "%d", &vector_ptr[j]);
-
+        
+        call_count++;
         recursive_quicksort(vector_ptr, 0, vector_len - 1);
+
+        fprintf(output_fp, "Swap count = %d \tCall count = %d\n", swap_count, call_count);
 
         for (int j = 0; j < vector_len; j++)
         {
             fprintf(output_fp, "%d ", vector_ptr[j]);
         }
         fprintf(output_fp, "\n");
+
+        swap_count = 0;
+        call_count = 0;
+
         free(vector_ptr);
     }
 }
