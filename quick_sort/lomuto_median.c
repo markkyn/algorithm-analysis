9b86c6d25fdeaa0d0ded281b056a81d52c952c47
markkyn <marcos.gabriel@ossirian.com>
2024-01-23 23:39:45 -0300

    fix: finalização do quicksort

--- quick_sort/lomuto_median.c
+++ quick_sort/lomuto_median.c
@@ -17,7 +17,6 @@ void swap(int32_t *array_ptr, int source_pos, int dest_pos)
 
 int32_t median(int a, int b, int c, int32_t n, int32_t lo)
 {
-    call_count++;
 
     if ((a >= b && a <= c) || (a <= b && a >= c))
         return lo + (n / 4); // a
@@ -27,26 +26,78 @@ int32_t median(int a, int b, int c, int32_t n, int32_t lo)
         return lo + (3 * n / 2); // c
 }
 
-int32_t lomuto_median(int32_t* vector, int32_t low, int32_t high)
+int32_t lomuto_median(int32_t *vector, int32_t low, int32_t high)
 {
-  call_count++;
-  
-  int32_t n = high - low + 1;
-  
-  int32_t a = vector[low + (n / 4)], b = vector[low + (n / 2)], c = vector[low + (3 * n / 4)];
+    call_count++;
 
-  int32_t k = median(a, b, c, n low);
+    int32_t n = high - low + 1;
 
-  swap(vector, high, k)
-  
-  int32_t P = vector_ptr[high], x = low - 1, y = low;
+    int32_t a = vector[low + (n / 4)], b = vector[low + (n / 2)], c = vector[low + (3 * n / 4)];
 
-    for (y = i; y < j; y++)
-        if (vector_ptr[y] <= P)
-            swap(vector_ptr, ++x, y);
+    int32_t k = median(a, b, c, n, low);
 
-    swap(vector_ptr, ++x, high);
+    swap(vector, high, k);
 
-    return x;
-  
+    int32_t P = vector[high], x = low - 1, y = low;
+
+    for (y = low; y < high; y++)
+        if (vector[y] <= P)
+            swap(vector, ++x, y);
+
+    swap(vector, ++x, high);
+
+    return y;
+}
+
+void recursive_quicksort(int32_t *vector, int32_t start, int32_t end)
+{
+    if (start < end)
+    {
+        int32_t pivot = lomuto_median(vector, start, end);
+        // sorting by partitioning;
+
+        recursive_quicksort(vector, start, pivot-1);
+        recursive_quicksort(vector, pivot + 1, end);
+    }
+    return; // useless return (end recursion);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        perror("Quantidade incorreta de argumentos!");
+    }
+
+    FILE *input_fp = fopen(argv[1], "r");
+    FILE *output_fp = fopen(argv[2], "w");
+
+    int32_t vectors_count, vector_len;
+
+    fscanf(input_fp, "%d", &vectors_count);
+    for (int i = 0; i < vectors_count; i++)
+    {
+        fscanf(input_fp, "%d", &vector_len);
+
+        int32_t *vector_ptr = malloc(vector_len * sizeof(int32_t));
+
+        for (int j = 0; j < vector_len; j++)
+            fscanf(input_fp, "%d", &vector_ptr[j]);
+
+        recursive_quicksort(vector_ptr, 0, vector_len - 1);
+    printf("\nAqui\n");
+
+        fprintf(output_fp, "Swap count = %d \tCall count = %d\n", swap_count, call_count);
+
+        for (int j = 0; j < vector_len; j++)
+        {
+            fprintf(output_fp, "%d ", vector_ptr[j]);
+        }
+        fprintf(output_fp, "\n");
+
+        swap_count = 0;
+        call_count = 0;
+
+        free(vector_ptr);
+    }
 }
--- quick_sort/main.c
+++ quick_sort/main.c
@@ -7,6 +7,22 @@
 /* ------ Global ------ */
 uint32_t call_count = 0;
 
+typedef enum
+{
+    LP = 0,
+    LM,
+    LA,
+    HP,
+    HM,
+    HA,
+} Type;
+
+typedef struct Sort
+{
+    char *type;
+    int32_t call_count;
+} Sort_t;
+
 /* ------ Utils ------ */
 void swap(int32_t *array_ptr, int source_pos, int dest_pos)
 {
@@ -17,31 +33,23 @@ void swap(int32_t *array_ptr, int source_pos, int dest_pos)
     array_ptr[source_pos] = temp;
 }
 
-int32_t median(int a, int b, int c, int32_t n, int32_t lo)
+void swap_Sort(Sort_t *array_ptr, int source_pos, int dest_pos)
 {
-    if ((a >= b && a <= c) || (a <= b && a >= c))
-        return lo + (n / 4); // a
-    else if ((b >= a && b <= c) || (b <= a && b >= c))
-        return lo + (n / 2); // b
-    else
-        return lo + (3 * n / 2); // c
+    Sort_t temp = array_ptr[dest_pos];
+
+    array_ptr[dest_pos] = array_ptr[source_pos];
+    array_ptr[source_pos] = temp;
 }
 
-typedef enum
+int32_t median(int a, int b, int c, int32_t n, int32_t lo)
 {
-    LP = 0,
-    LM,
-    LA,
-    HP,
-    HM,
-    HA
-} Type;
-
-typedef struct Sort
-{
-    char *type;
-    int32_t call_count;
-} Sort;
+    if ((a > b && a < c) || (a < b && a > c))
+        return lo + (n / 4); // a
+    else if ((b > a && b < c) || (b < a && b > c))
+        return lo + (n / 2); // b
+    else
+        return lo + (3 * n / 4); // c
+}
 
 /* -------------------- */
 /* ------ Hoare ------- */
@@ -50,7 +58,6 @@ typedef struct Sort
 int32_t hoare_partition(int32_t *vector, int32_t low, int32_t high) // i = first position ; j = last position
 {
     // aka: HP(x)
-    call_count++;
 
     // Arbitrary Pivot.
     int32_t P = vector[low], x = low - 1, y = high + 1;
@@ -71,7 +78,6 @@ int32_t hoare_partition(int32_t *vector, int32_t low, int32_t high) // i = first
 int32_t hoare_median(int32_t *vector, int32_t low, int32_t high) // i = first position ; j = last position
 {
     // aka: HM(x)
-    call_count++;
 
     int32_t n = high - low + 1;
 
@@ -86,25 +92,26 @@ int32_t hoare_median(int32_t *vector, int32_t low, int32_t high) // i = first po
 
     while (true)
     {
-        while (vector[--y] > P)
-            ;
         while (vector[++x] < P)
             ;
-        if (x < y)
-            swap(vector, x, y);
-        else
+        while (vector[--y] > P)
+            ;
+        if (x >= y)
             return y;
+
+        swap(vector, x, y);
     }
 }
 
-int32_t hoare_rand(int32_t *vector, int32_t i, int32_t j)
+int32_t hoare_rand(int32_t *vector, int32_t low, int32_t high)
 {
     // aka: HA(x)
-    call_count++;
 
-    swap(vector, i, (i + abs(i) % (j - i + 1)));
+    int32_t k = (low + (abs(vector[low]) % (high - low + 1)));
 
-    return hoare_partition(vector, i, j);
+    swap(vector, low, k);
+
+    return hoare_partition(vector, low, high);
 }
 
 /* -------------------- */
@@ -114,7 +121,6 @@ int32_t hoare_rand(int32_t *vector, int32_t i, int32_t j)
 int32_t lomuto_partition(int32_t *vector_ptr, int32_t i, int32_t j)
 {
     // aka: LP(x)
-    call_count++;
 
     int32_t P = vector_ptr[j], x = i - 1, y = i;
 
@@ -130,17 +136,25 @@ int32_t lomuto_partition(int32_t *vector_ptr, int32_t i, int32_t j)
 int32_t lomuto_rand(int32_t *vector, int32_t i, int32_t j)
 {
     // aka: LA(x)
-    call_count++;
 
-    swap(vector, j, (i + abs(i) % (j - i + 1)));
+    swap(vector, j, (i + (abs(vector[i]) % (j - i + 1))));
 
     return lomuto_partition(vector, i, j);
 }
 
-int32_t lomuto_median(int32_t *vector_ptr, int32_t i, int32_t j)
+int32_t lomuto_median(int32_t *vector, int32_t low, int32_t high)
 {
     // aka: LM(x)
-    call_count++;
+
+    int32_t n = high - low + 1;
+
+    int32_t a = vector[low + (n / 4)], b = vector[low + (n / 2)], c = vector[low + (3 * n / 4)];
+
+    int32_t k = median(a, b, c, n, low);
+
+    swap(vector, high, k);
+
+    return lomuto_partition(vector, low, high);
 }
 
 /* ----------------------- */
@@ -148,6 +162,7 @@ int32_t lomuto_median(int32_t *vector_ptr, int32_t i, int32_t j)
 /* ----------------------- */
 void recursive_quicksort(int32_t *vector_ptr, int32_t low, int32_t high, Type type)
 {
+    call_count++;
     int32_t pivot;
 
     if (low < high)
@@ -163,6 +178,7 @@ void recursive_quicksort(int32_t *vector_ptr, int32_t low, int32_t high, Type ty
 
         case LM:
             pivot = lomuto_median(vector_ptr, low, high);
+            // sorting by partitioning;
 
             recursive_quicksort(vector_ptr, low, pivot - 1, type);
             recursive_quicksort(vector_ptr, pivot + 1, high, type);
@@ -201,6 +217,28 @@ void recursive_quicksort(int32_t *vector_ptr, int32_t low, int32_t high, Type ty
             break;
         }
     }
+}
+
+/* ----------------------- */
+/* -------- Call --------- */
+/* ----------------------- */
+
+int32_t insertion_sort(Sort_t *sort, int32_t n)
+{
+    int i, j;
+    Sort_t chave;
+
+    for (i = 1; i < n; i++) {
+        chave = sort[i];
+        j = i - 1;
+
+        while (j >= 0 && sort[j].call_count > chave.call_count) {
+            sort[j + 1] = sort[j];
+            j = j - 1;
+        }
+        sort[j + 1] = chave;
+    }
+    return 0; 
 }
 
 /* ----------------------- */
@@ -248,15 +286,16 @@ int main(int argc, char *argv[])
         ha_ptr = malloc(vector_len * sizeof(int32_t));
         memcpy(ha_ptr, lp_ptr, vector_len * sizeof(int32_t));
 
-        Sort sort[6];
+        Sort_t sort[6];
 
+        call_count = 0;
         sort[LP].type = "LP";
         recursive_quicksort(lp_ptr, 0, vector_len - 1, LP);
         sort[LP].call_count = call_count;
 
         call_count = 0;
         sort[LM].type = "LM";
-        // recursive_quicksort(lm_ptr, 0, vector_len - 1, LM);
+        recursive_quicksort(lm_ptr, 0, vector_len - 1, LM);
         sort[LM].call_count = call_count;
 
         call_count = 0;
@@ -276,15 +315,20 @@ int main(int argc, char *argv[])
 
         call_count = 0;
         sort[HA].type = "HA";
-        recursive_quicksort(ha_ptr, 0, vector_len - 1, LP);
+        recursive_quicksort(ha_ptr, 0, vector_len - 1, HA);
         sort[HA].call_count = call_count;
 
-        // TODO: SORT analysis call_count
+        for (int j = 0; j < vector_len; j++)
+            printf("%d ", ha_ptr[j]);
+        printf("\n");
 
-        fprintf(output_fp, "%d: N(%d)", i, vector_len);
+        fprintf(output_fp, "%d: N(%d) ", i, vector_len);
 
-        for (int j = 0; j < 6; j++) // TODO: enum to str
+        insertion_sort(sort, 6);
+
+        for (int j = 0; j < 6; j++)
             fprintf(output_fp, "%s(%d) ", sort[j].type, sort[j].call_count);
+
         fprintf(output_fp, "\n");
     }
 }
